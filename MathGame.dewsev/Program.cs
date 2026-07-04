354acfbc09ab3f4327d112318d5e03d7606bcd25
Seweryn <git.freedom557@passinbox.com>
2026-07-04 15:46:35 +0200

    Add GetNumericInputFromUser method; Clean up

--- MathGame.dewsev/Program.cs
+++ MathGame.dewsev/Program.cs
@@ -1,55 +1,41 @@
+// TODO: Display goodbye message when quitting the game
+// TODO: Is ArgumentException a good Exception type?
+// TODO: Extract all input gathering to separate methods
+// TODO: Intercept keys in the menus, so the user does not need to press ENTER every time (maybe)
+
+
+const int questionCount = 10;
 Random random = new Random();
 
+// TODO: You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+// Keep score from each game inside a list, gameHistory[0] -> game 1, etc...
+// Keep operation type for each game
+List<int> gameHistory = [];
+
+OperationType operationType = OperationType.Addition;
 int score = 0;
-int questionCount = 15;
 bool randomizedOperations = false;
+bool quit = false;
+string? readResult;
 
-Play();
+Run();
 
 
-void Play()
+void Run()
 {
-    DisplayWelcomeMessage();
- 
-    OperationType operationType = GetOperationTypeChoiceFromUser();
-    
-    Console.Clear();
-    for (int i = 0; i < questionCount; i++)
+    while (!quit)
     {
-        (int operand1, int operand2) = GetRandomOperands(operationType);
-        DisplayOperationString(operationType, operand1, operand2);
-
-        string? readResult;
-        do
-        {
-            readResult = Console.ReadLine()?.Trim();
-            bool isValidInteger = int.TryParse(readResult, out int answer);
-            if (isValidInteger)
-            {
-                CheckAnswer(operationType, operand1, operand2, answer);
-                if (randomizedOperations)
-                {
-                    operationType = GetRandomOperationType(operationType);
-                }
-            }
-            else
-            {
-                ClearCurrentConsoleLine();
-                WriteColoredLine("Provide a valid integer.", ConsoleColor.Red);
-            
-                Console.Write($"{operand1} + {operand2} = ");
-                readResult = "";
-            }
-        } while (string.IsNullOrEmpty(readResult));
+        Console.Clear();
+        MainMenu();
+        Console.Clear();
+        PlayGame();
+        Console.Clear();
+        GameOver();
     }
-    
-    Console.WriteLine($"\nYour score: {score}/{questionCount}");
-    Console.WriteLine("Press any key to exit.");
-    Console.ReadLine();
 }
 
 
-(int, int) GetRandomOperands(OperationType operationType)
+(int, int) GetRandomOperands()
 {
     int operand1 = random.Next(1, 101);
     int operand2 = random.Next(1, 101);
@@ -58,7 +44,6 @@ void Play()
     {
         while (operand1 % operand2 != 0)
         {
-            operand1 = random.Next(1, 101);
             operand2 = random.Next(1, 101);
         }
     }
@@ -67,40 +52,132 @@ void Play()
 }
 
 
-void DisplayWelcomeMessage()
+void PlayGame()
+{
+    for (int i = 0; i < questionCount; i++)
+    {
+        (int operand1, int operand2) = GetRandomOperands();
+
+        bool invalidAnswer = true;
+        while (invalidAnswer)
+        {
+            try
+            {
+                DisplayOperationString(operand1, operand2);
+                int answer = GetNumericInputFromUser();
+
+                CheckAnswer(operand1, operand2, answer);
+                if (randomizedOperations)
+                {
+                    operationType = GetRandomOperationType();
+                }
+
+                invalidAnswer = false;
+            }
+            catch (ArgumentException)
+            {
+                invalidAnswer = true;
+                ClearCurrentConsoleLine();
+                WriteColoredLine("Please provide a valid integer.", ConsoleColor.Red);
+            }    
+        }
+    }
+}
+
+
+void SetupNewGame()
+{
+    score = 0;
+    randomizedOperations = false;
+    operationType = GetOperationTypeChoiceFromUser();
+}
+
+
+void MainMenu()
 {
     Console.WriteLine("Welcome to the MathGame!");
-    Console.WriteLine($"You need to answer {questionCount} questions.");
+    Console.WriteLine($"You can test your *quick maths skills* by answering {questionCount} questions.");
+    Console.WriteLine("\n1.Start new game");
+    Console.WriteLine("2.Show game history");
+    Console.WriteLine("3.Exit\n");
+
+    try
+    {
+        Console.Write("Your choice: ");
+        int choice = GetNumericInputFromUser(1, 3);
+
+        switch (choice)
+        {
+            case 1:
+                SetupNewGame();
+                break;
+            case 2:
+                // DisplayGameHistory();
+                break;
+            case 3:
+                quit = true;
+                break;
+        }
+    }
+    catch (ArgumentException ex)
+    {
+        WriteColoredLine(ex.Message, ConsoleColor.Red);
+    }    
+}
+
+
+void GameOver()
+{
+    // TODO: Add score to gameHistory
+    // TODO: Colorize the score based on how well the game went
+    Console.WriteLine($"Your score: {score}/{questionCount}");
+    Console.WriteLine("Press any key to go back to main menu.");
+    Console.WriteLine("Type EXIT to quit.");
+
+    readResult = Console.ReadLine()?.ToLower().Trim();
+    if (readResult == "exit")
+    {
+        quit = true;
+    }
+}
+
+
+int GetNumericInputFromUser(int min = int.MinValue, int max = int.MaxValue)
+{
+    while (true)
+    {
+        string? input = Console.ReadLine()?.Trim();
+        bool isNumericInput = int.TryParse(input, out int numericInput);
+        if (isNumericInput && numericInput >= min && numericInput <= max)
+        {
+            return numericInput;
+        }
+        throw new ArgumentException("Invalid input.");
+    }
 }
 
 
 OperationType GetOperationTypeChoiceFromUser()
 {
-    Console.WriteLine("\nChoose an operation type:");
-    Console.WriteLine("  1.Addition");
-    Console.WriteLine("  2.Subtraction");
-    Console.WriteLine("  3.Multiplication");
-    Console.WriteLine("  4.Division");
-    Console.WriteLine("  5.Random\n\n");
+    Console.WriteLine("Choose an operation type:\n");
+    Console.WriteLine("1.Addition");
+    Console.WriteLine("2.Subtraction");
+    Console.WriteLine("3.Multiplication");
+    Console.WriteLine("4.Division");
+    Console.WriteLine("5.Random\n\n");
     
     while (true)
     {
         ClearCurrentConsoleLine();
-        Console.Write("Your choice: ");
-        string? choice = Console.ReadLine()?.Trim();
-
-        bool isValidInteger = int.TryParse(choice, out int integerChoice);
-        if (isValidInteger && integerChoice is > 0 and < 6)
+        int choice = GetNumericInputFromUser(1, 5);
+        return choice switch
         {
-            return integerChoice switch
-            {
-                1 => OperationType.Addition,
-                2 => OperationType.Subtraction,
-                3 => OperationType.Multiplication,
-                4 => OperationType.Division,
-                5 => SetupRandomOperations(),
-            };
-        }
+            1 => OperationType.Addition,
+            2 => OperationType.Subtraction,
+            3 => OperationType.Multiplication,
+            4 => OperationType.Division,
+            5 => SetupRandomOperations(),
+        };
     }
 }
 
@@ -112,14 +189,16 @@ OperationType SetupRandomOperations()
 }
 
 
-OperationType GetRandomOperationType(OperationType? currentOperationType = null)
+OperationType GetRandomOperationType()
 {
     while (true)
     {
         int randomIndex = random.Next(Enum.GetValues<OperationType>().Length);
         OperationType randomOperation = (OperationType)randomIndex;
-
-        if (randomOperation != currentOperationType)
+        
+        // TODO: Fix this, gives the same operation few times in a row
+        // the gameHistory.Count check is wrong
+        if (gameHistory.Count == 0 || randomOperation != operationType)
         {
             return randomOperation;
         }
@@ -127,13 +206,13 @@ OperationType GetRandomOperationType(OperationType? currentOperationType = null)
 }
 
 
-void CheckAnswer(OperationType operationType, int operand1, int operand2, int answer)
+void CheckAnswer(int operand1, int operand2, int answer)
 {
     ClearCurrentConsoleLine();
 
     int expectedResult = ComputeExpectedResult(operand1, operand2, operationType);
     
-    DisplayOperationString(operationType, operand1, operand2, answer);
+    DisplayOperationString(operand1, operand2, answer);
     Console.Write("\t");
     if (expectedResult == answer)
     {
@@ -147,7 +226,7 @@ void CheckAnswer(OperationType operationType, int operand1, int operand2, int an
 }
 
 
-void DisplayOperationString(OperationType operationType, int operand1, int operand2, int? result = null)
+void DisplayOperationString(int operand1, int operand2, int? result = null)
 {
     char op = operationType switch
     {
